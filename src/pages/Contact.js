e3ee84ef7a5362c913619d39f49c601d01acb400
Jacobschoenbauer <jhwt65@gmail.com>
2023-07-11 09:21:42 -0500

    worked on contact info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
 import data from "../Data/Data.json";
-import "../App.css"
+import "../App.css";
 export default class Contact extends Component {
-    render() {
-      const {
-        about: {  telephone, email },
-      } = data;
-  
-    
+  render() {
+    const {
+      about: { telephone, email },
+    } = data;
+
     return (
-        <div>
-         <a className="phone" href="tel:651-500-1758">
-                {telephone}
-              </a>
-              <a className="email" href="mailto:davesnewlookservices@gmail.com">
-                {email}
-              </a> 
-              
-        </div>
-    )
-}
+      <div>
+       <article className="contact-flex">
+        <section className="contact-phone">
+          <a className="phone" href="tel:651-500-1758">
+            {telephone}
+          </a>
+        </section>
+        <section className="contact-email">
+        <a className="email" href="mailto:davesnewlookservices@gmail.com">
+          {email}
+        </a>
+        </section>
+        </article>
+      </div>
+    );
+  }
 }
